f95941831fa4a3c5ff1131eaf23d3ae4bd5ea16a
Henrynunez112 <henrynunez@pursuit.org>
2020-03-20 15:04:28 -0400

    fetching Data

--- counter-react/src/App.js
+++ counter-react/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import MouseContaniner from './components/MouseContaniner';
+import DataFetching from './components/DataFetching';
+// import MouseContaniner from './components/MouseContaniner';
 // import ClassCounterOne from './components/ClassCounterOne';
 // import UseEffectHookCounterOne from './components/UseEffectHookCounterOne';
 // import HookCounterFour from './components/HookCounterFour';
@@ -10,6 +11,8 @@ import MouseContaniner from './components/MouseContaniner';
 // import HookCounterThree from './components/HookCounterThree';
 // import ClassMouse from './components/ClassMouse'
 // import HookMouse from './components/HookMouse';
+// import IntervalClassCounter from './components/IntervalClassCounter'
+// import IntervalHookCounter from './components/IntervalHookCounter'
 
 function App() {
   return (
@@ -23,7 +26,10 @@ function App() {
       {/* <UseEffectHookCounterOne /> */}
       {/* <ClassMouse /> */}
       {/* <HookMouse /> */}
-      <MouseContaniner />
+      {/* <MouseContaniner /> */}
+      {/* <IntervalClassCounter />
+      <IntervalHookCounter /> */}
+      <DataFetching />
 
     </div>
   );
