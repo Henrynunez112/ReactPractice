a87815e5c1d0868efbf70c6224bc216e73264854
Henrynunez112 <henrynunez@pursuit.org>
2020-03-20 12:30:10 -0400

    recap on setState

--- counter-react/src/components/HookCounterFour.js
+++ counter-react/src/components/HookCounterFour.js
@@ -25,3 +25,11 @@ function HookCounterFour() {
 }
 
 export default HookCounterFour
+
+//The useState hook lets you add state to functional components
+//In classes, the state is always an object.
+//With the useState hook, the state doesn't have to be an object.
+//The useState hook returns an arrat with 2 elements.
+//The first element is the current value of the state, and the second element is a state setter function.
+//New state value depends on the previous state value? You can pass a function to the setter function.
+//When dealing with objects or arrays, make sure to sprea your state variable and then call the setter function.
